9580a2ea58c27587dfa5c4209c3f5d55ba7fc014
unknown <adiefsal@gmail.com>
2024-08-16 16:21:19 +0700

    initial commit

--- database.sql
+++ database.sql
@@ -6,3 +6,23 @@ CREATE TABLE categories (
     description text,
     created_at TIMESTAMP ,
 ) ENGINE=InnoDB;
+
+
+CREATE TABLE counter (
+    id INT  NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+    counter int NOT NULL DEFAULT 0
+) ENGINE=InnoDB
+
+INSERT INTO counter (counter ) VALUES(
+     1
+);
+
+CREATE TABLE products (
+    id  VARCHAR(100) NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description text NULL,
+    price int NOT NULL,
+    category_id VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories (id)
+)ENGINE=InnoDB
--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -8,11 +8,14 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertNotNull;
+
 class QueryBuilderTest extends TestCase
 {
     public function setUp():void {
         parent::setUp();
-        DB::table("categories")->truncate();
+        DB::table("products")->delete();
+        DB::table("categories")->delete();
     }
 
     public function insertCategories() {
@@ -20,10 +23,9 @@ class QueryBuilderTest extends TestCase
             "id" => "GADGET",
             "name" => "Tablet"
         ]);
-        
         DB::table("categories")->insert([
             "id" => "OTOMOTIF",
-            "name" => "RX - King"
+            "name" => "Motor Bicycle"
         ]);
         
         DB::table("categories")->insert([
@@ -33,21 +35,154 @@ class QueryBuilderTest extends TestCase
 
         DB::table("categories")->insert([
             "id" => "BOOKS",
-            "name" => "Stoicism"
+            "name" => "Knowledge"
         ]);
     }
 
-    public function testUpdate() {
+    public function insertProducts() {
         $this->insertCategories();
 
-        DB::table("categories")->where("id", "=", "GADGET")->update([
-            "name" => "Xiaomi"
+        DB::table("products")->insert([
+            'id' => "1",
+            "name" => "Xiaomi Redmi Note 8 Pro",
+            "category_id" => "SMARTPHONE",
+            "price" => 2000000,
         ]);
 
-        $collection = DB::table("categories")->where("name", "=", "Handphone")->get();
-        self::assertCount(1, $collection);
-        $collection->each(function ($item) {
+        DB::table("products")->insert([
+            'id' => "2",
+            "name" => "Xiaomi Redmi Note 9 Pro",
+            "category_id" => "SMARTPHONE",
+            "price" => 2500000,
+        ]);
+    }
+
+    public function testQueryBuilderJoin() {
+        $this->insertProducts();
+
+        $collection = DB::table("products")
+        ->join("categories", "products.category_id", "=", "categories.id")
+        ->select("products.id", "products.name", "products.price", "categories.name as category_name")
+        ->get();
+        
+        self::assertCount(2, $collection);
+        $collection->each(function ($item){
             Log::info(json_encode($item));
         });
     }
+
+    public function testOrdering() {
+        $this->insertProducts();
+
+        $collection = DB::table("products")->whereNotNull("id")
+        ->orderBy("name", "asc")->get();
+        
+        self::assertCount(2, $collection);
+        $collection->each(function ($item){
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function testPaging() {
+        $this->insertCategories();
+
+        $collection = DB::table("categories")
+        ->skip(0)
+        ->take(1)
+        ->get();
+
+        self::assertCount(1, $collection);
+        $collection->each(function ($item){
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function insertManyCategories() {
+        for ($i = 0; $i < 100; $i++) {
+            DB::table("categories")->insert([
+                    "id" => "categories - $i",
+                    "name" => "name - $i"
+                ]);
+        }
+    }
+
+    public function testQueryBuilderChunkResults(){
+        $this->insertManyCategories();
+
+        DB::table("categories")->orderBy("id")
+        ->chunk(10, function ($categories){
+            Log::info("Start Chunk");
+            $this->assertNotNull($categories);
+            $categories->each(function ($category) {
+                Log::info(json_encode($category));
+            });
+            Log::info("End Chunk");
+        });
+    }
+
+
+    // public function testUpdate() {
+    //     $this->insertCategories();
+
+    //     DB::table("categories")->where("id", "=", "GADGET")->update([
+    //         "name" => "Xiaomi"
+    //     ]);
+
+    //     $collection = DB::table("categories")->where("name", "=", "Handphone")->get();
+    //     self::assertCount(1, $collection);
+    //     $collection->each(function ($item) {
+    //         Log::info(json_encode($item));
+    //     });
+    // }
+
+    // public function testUpSert() {
+    //     DB::table("categories")->updateOrInsert([
+    //         'id' => "VOUCHER"
+    //     ], [
+    //         "name" => "voucher",
+    //         "description" => "Ticket and Voucher"
+    //     ]);
+
+    //     $collection = DB::table("categories")->where("id", "=", "VOUCHER")->get();
+    //     self::assertCount(1, $collection);
+    //     $collection->each(function ($item) {
+    //         Log::info(json_encode($item));
+    //     });
+    // }
+
+    // public function testIncrement() {
+    //     DB::table("counter")->where("id", "=", 1)->increment("counter", 1);
+
+    //     $collection = DB::table("counter")->where("id", "=", 1)->get();
+    //     self::assertCount(1, $collection);
+    //     $collection->each(function ($item) {
+    //         Log::info(json_encode($item));
+    //     });
+    // }
+
+    // public function testQueryBuilderWhere() {
+    //     $this->insertCategories();
+    //     $collection = DB::table("categories")->where("id","=", "GADGET")->get();
+    //     $this->assertCount(1, $collection);
+    //     $collection->each(function ($item) {
+    //         Log::info(json_encode($item));
+    //     });
+    // }
+
+    // public function testDelete() {
+    //     $this->insertCategories();
+    //     DB::table("categories")->where("id", "=", "GADGET")->delete();
+
+    //     $collection = DB::table("categories")->where("id", "=", "GADGET")->get();
+    //     $collection2 = DB::table("categories")->where("id", "=", "OTOMOTIF")->get();
+
+    //     self::assertCount(0, $collection);
+    //     self::assertCount(1, $collection2);
+        
+    // }
+
+
+
+
+
 }
